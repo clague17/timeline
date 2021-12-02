a3318cc9bde49169b9790d66b28488168e688ac3
clague <lclague9@gmail.com>
2021-12-02 23:51:10 +0100

    can authenticate with spotify

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import NextLink from "next/link";
+import styles from "../styles/Home.module.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+// spotify things
+
+const getSpotify = "/api/get_data";
+
+const fetchSpotify = async () => {
+  // const { data, error } = useSWR("/api/user", fetcher);
+  console.log("we're here");
+  const res = await fetch("/api/get_data");
+  console.log(res);
+};
 
 const Home: NextPage = () => {
   return (
@@ -13,12 +26,13 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <Button onClick={fetchSpotify}>Connect with Spotify</Button>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +73,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
